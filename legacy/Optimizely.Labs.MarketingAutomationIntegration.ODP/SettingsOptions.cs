f049d75a1a3cf8fe77f4e5b39c031ef9368a182a
Daniel Isaacs <daniel.isaacs@episerver.com>
2022-08-18 03:20:00 +0000

    Make ODP endpoint configurable

--- legacy/Optimizely.Labs.MarketingAutomationIntegration.ODP/SettingsOptions.cs
+++ legacy/Optimizely.Labs.MarketingAutomationIntegration.ODP/SettingsOptions.cs
@@ -6,9 +6,12 @@ namespace Optimizely.Labs.MarketingAutomationIntegration.ODP
     {
         static SettingsOptions()
         {
+            OdpBaseEndPoint = ConfigurationManager.AppSettings["ma-odp-odpbaseendpoint"];
             CustomerObjectName = ConfigurationManager.AppSettings["ma-odp-customerobjectname"];
             APIKey = ConfigurationManager.AppSettings["ma-odp-apikey"];
         }
+
+        public static string OdpBaseEndPoint { get; }
 
         public static string CustomerObjectName { get; }
 
